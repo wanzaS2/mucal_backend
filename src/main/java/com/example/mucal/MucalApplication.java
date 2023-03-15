f04c3c79344fb2140d638a94cf748c394cd63c64
wanzaS2 <91421644+wanzaS2@users.noreply.github.com>
2023-03-16 04:57:37 +0900

    서버 구축 1st commit

---
+++ src/main/java/com/example/mucal/MucalApplication.java
@@ -0,0 +1,13 @@
+package com.example.mucal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MucalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MucalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mucal/MucalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mucal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MucalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
